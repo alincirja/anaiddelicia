a195b205676f0eda527e46974321cd7e81d4853d
Alin Cirja <alincirja07@gmail.com>
2019-01-25 20:28:17 +0200

    sters menu item

--- dist/templates/common/header.php
+++ dist/templates/common/header.php
@@ -40,7 +40,6 @@
                                 <li><a href="#">Sfaturi Culinare</a></li>
                                 <li><a href="#">Generare Meniu</a></li>
                                 <li><a href="#">Topuri</a></li>
-                                <li><a href="#">De Post</a></li>
                                 <li><a href="#">Contact</a></li>
                             </ul>
                         </nav>
