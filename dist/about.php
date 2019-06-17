7796e8fb95c21a2af202fb4b3bcb4d6b1032aa43
DianaLuciana <diana.cirja97@gmail.com>
2019-06-17 17:23:49 +0300

    statistici

---
+++ dist/about.php
@@ -0,0 +1,56 @@
+<?php
+   
+    $pageTitle = "Despre";
+
+    require_once("templates/common/head.php");
+?>
+
+<?php
+    require_once("templates/common/header.php");
+?>
+<section id="main" class="main-content content-page">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-6">
+                <img src="../dist/img/bucatarie.jpg" class="img-fluid">
+            </div>
+            <div class="col-12 col-md-6">
+                <h2>Bucătăria noastră</h2>
+                <h4>Bine ați venit in bucătăria noastră! Aici are loc magia. Aici, impreuna cu cei mai iscusiți bucătari,
+                preparăm cu dragoste mâncarea pe care dumneavoastra o să o serviți la un eveniment special din viața 
+                dumneavoastră, cum ar fi nunta, botez, zi de naștere sau majorat.
+                Vă asigurăm că bucătarii noștri folosesc cele mai noi ustensile și cele mai proaspete ingrediente pentru a prepara 
+                mâncărurile alese de dumneavoastră.</h4>
+            </div>
+        </div>
+        <div class ="my-5"></div>
+        <div class="row">
+            <div class="col-12 col-md-6">
+                <h1>Bucătarii nostri</h1>
+                <h4>Bucatar sef Silvia</h4>
+            </div>
+            <div class="col-12 col-md-6">
+                <img src="../dist/img/bucatarie.jpg" class="img-fluid">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 col-md-6">
+                <h4>Ajutor de bucatar Silvia</h4>
+            </div>
+            <div class="col-12 col-md-6">
+                <img src="../dist/img/bucatar2.jpg" class="img-fluid">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 col-md-6">
+                <h4>Ajutor de bucatar Silvia</h4>
+            </div>
+            <div class="col-12 col-md-6">
+                <img src="../dist/img/ajutorbucatar.jpg" class="img-fluid">
+            </div>
+        </div>
+    </div>
+</section>
+<?php
+    require_once("templates/common/footer.php");
+?>
